70e4b0940500334354163e7367d07c15ed5cc67e
Nisa Nabila <107998430+nisanabil@users.noreply.github.com>
2022-11-19 11:34:37 +0700

    Nisa Nabila Praktikum Laravel FINISH

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -8,12 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     use HasFactory;
-
-    /**
-     * fillable
-     *
-     * @var array
-     */
     protected $fillable = [
         'image', 'title', 'content'
     ];
